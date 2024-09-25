9a7b7d8d23382890def233c2ad4dc029ed2ef4c9
Gabriel <biiel.adtr@gmail.com>
2024-09-25 15:52:05 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import openpyxl
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+planilha_clientes = openpyxl.load_workbook('dados_clientes.xlsx')
+pagina_clientes = planilha_clientes['sheet1']
+driver = webdriver.Chrome()
+driver.get('') #Inserir a URL depois
+
+for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
+    nome, valor, cpf, vencimento = linha
+
+    
+    sleep(5)
+    campo_pesquisa = driver.find_element(By.XPATH,"//input[@id= 'cpInput']")
+    sleep(1)
+    campo_pesquisa.clear()
+    campo_pesquisa.send_keys(cpf)
+    sleep(1)
+
+    botao_pesquisar = driver.find_element(By.XPATH, "//button[@class= 'btn btn-custom btn-lg btn-block mt-3]")
+    sleep(1)
+    botao_pesquisar.click()
+    sleep(4)
+
+    status = driver.find_element(By.XPATH,"//span[@id='statusLabel']")
+    if status.text == 'em dia':
+        data_pagamento = driver.find_element(By.XPATH, "//p[@id='paymentDate']")
+        metodo_pagamento = driver.find_element(By.XPATH, "//p[@id='paymentMethod']")
+        
+        data_pagamento_limpo = data_pagamento.text.split()[3]
+        metodo_pagamento_limpo = metodo_pagamento.text.split()[3]
+        
+        planilha_fechamento = openpyxl.load_workbook('planilha fechamento.xlsx')
+        pagina_fechamento = planilha_fechamento['Sheet1']
+
+
+        pagina_fechamento.append([nome, valor, cpf, vencimento, 'em dia', data_pagamento_limpo, metodo_pagamento_limpo])
+        pagina_fechamento.save('planilha fechamento.xlsx')
+    else:
+
+        planilha_fechamento = openpyxl.load_workbook('planilha fechamento.xlsx')
+        pagina_fechamento = planilha_fechamento['Sheet1']
+
+        pagina_fechamento.append([nome, valor, cpf, vencimento, 'pendente' ])
